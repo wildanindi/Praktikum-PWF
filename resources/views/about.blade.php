8eb87d8517c55dcd36e42eb387a48ffbce1cb87b
wildanindi2@gmail.com <wildanindi2@gmail.com>
2026-03-03 10:35:24 +0700

    Commit tugas ke dua installation breeze

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 leading-relaxed">
+                    <p>Nama : Muhammad Wildan Indi Naufal</p>
+                    <p>NIM : 20230140020</p>
+                    <p>Program Studi : Teknologi Informasi</p>
+                    <p>Hobi : Ngopi</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
